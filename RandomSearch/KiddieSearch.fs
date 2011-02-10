b804a42ab02a6cc8203f04dc2d50447855517387
Jon Newton <jon.newton@gmail.com>
2011-02-09 21:05:05 -0700

    Initial checkin of RandomSearch examples with F# for clever algorithms web book

---
+++ RandomSearch/KiddieSearch.fs
@@ -0,0 +1,16 @@
+﻿open System
+
+let makeRands qty min max = // Just think if I imported this!!!
+    let rand = new Random()
+    seq {for i in 1..qty -> rand.Next(min, max)}
+
+let searchSpace = Seq.toArray (makeRands 10 -5 5)
+
+let cost i = i * i * i
+
+let search (space:int[]) samples =
+    let pick best i = if cost space.[i] < cost best then space.[i] else best
+    let rands = makeRands samples 0 space.Length
+    Seq.fold pick space.[Seq.head rands] rands
+
+printf "Search space = %A\nminimum found = %d\n" searchSpace (search searchSpace 3)
---
+++ RandomSearch/ProSearch.fs
@@ -0,0 +1,23 @@
+﻿open System
+open System.IO
+
+let numFile = @"C:\Numbers.bin"
+
+let cost i = i
+
+let genFile qty min max file =
+    let rand = new Random()
+    use out = new BinaryWriter(File.Create(file))
+    let nums = seq {for i in 1 .. qty -> rand.Next(min, max)}
+    nums |> Seq.iter out.Write
+
+let searchFile file samples =
+    use input = new BinaryReader(File.OpenRead(file))
+    let rand = new Random()
+    let bounce = fun () -> input.BaseStream.Seek((int64 (rand.Next(0, int input.BaseStream.Length / 4) * 4), SeekOrigin.Begin)) |> ignore
+    let nums = seq{for i in 1 .. samples -> bounce() ; input.ReadInt32()}
+    let pick best i = if cost i < cost best then i else best
+    nums |> Seq.fold pick (Seq.head nums)
+
+genFile 10000000 0 1000000 numFile
+printf "minimum found = %d\n" (searchFile numFile 1000)
