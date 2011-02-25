5d393f7d0cbe3ccedc5ab1532e6e407a97b0e422
Jon Newton <jon.newton@gmail.com>
2011-02-25 00:34:07 -0700

    Added Temp curve plus stochastic hill climbing

--- RandomSearch/KiddieSearch.fs
+++ RandomSearch/KiddieSearch.fs
@@ -1,16 +1,27 @@
-﻿open System
+﻿module CleverAlgorithms.RandomSearch
 
-let makeRands qty min max = // Just think if I imported this!!!
+open System
+
+let makeRandSeq qty min max = // Just think if I imported this!!!
     let rand = new Random()
     seq {for i in 1..qty -> rand.Next(min, max)}
-
-let searchSpace = Seq.toArray (makeRands 10 -5 5)
 
 let cost i = i * i * i
 
 let search (space:int[]) samples =
     let pick best i = if cost space.[i] < cost best then space.[i] else best
-    let rands = makeRands samples 0 space.Length
-    Seq.fold pick space.[Seq.head rands] rands
+    let randIndexes = makeRandSeq samples 0 space.Length
+    Seq.fold pick space.[Seq.head randIndexes] randIndexes
 
-printf "Search space = %A\nminimum found = %d\n" searchSpace (search searchSpace 3)
+let printSearch space samples =
+    printf "Search space = %A\nminimum found = %d\n" space (search space samples)
+
+let testFirst = [|-5..5|]
+let testLast = [|5..-1..-5|]
+let testMid = List.toArray ([0..5] @ -5 :: [-4..-1])
+let searchSpace = Seq.toArray (makeRandSeq 10 -5 5)
+
+printSearch testFirst 10000
+printSearch testLast 10000
+printSearch testMid 10000
+printSearch searchSpace 3
--- RandomSearch/ProSearch.fs
+++ RandomSearch/ProSearch.fs
@@ -14,10 +14,12 @@ let genFile qty min max file =
 let searchFile file samples =
     use input = new BinaryReader(File.OpenRead(file))
     let rand = new Random()
-    let bounce = fun () -> input.BaseStream.Seek((int64 (rand.Next(0, int input.BaseStream.Length / 4) * 4), SeekOrigin.Begin)) |> ignore
+    let randPosition = fun () -> (int64 (rand.Next(0, int input.BaseStream.Length / 4) * 4))
+    let bounce = fun () -> input.BaseStream.Seek(randPosition(), SeekOrigin.Begin) |> ignore
     let nums = seq{for i in 1 .. samples -> bounce() ; input.ReadInt32()}
     let pick best i = if cost i < cost best then i else best
     nums |> Seq.fold pick (Seq.head nums)
 
-genFile 10000000 0 1000000 numFile
-printf "minimum found = %d\n" (searchFile numFile 1000)
+printf "Generating file...\n"
+genFile (int 1e8) 0 (int 1e6) numFile
+printf "minimum found = %d\n" (searchFile numFile 100000)
